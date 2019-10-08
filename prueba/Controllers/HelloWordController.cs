b7e8940ccd9c0411f161c55cf21a9cbe56f47aa4
piero.granda@hotmail.com <piero.granda@hotmail.com>
2019-10-07 19:06:36 -0500

    Agregar archivos de proyecto.

---
+++ prueba/Controllers/HelloWordController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prueba.Controllers
+{
+    public class HelloWordController : Controller
+    {
+        // GET: HelloWord
+        public string Index1() {
+            return "this  is  <b> default</b>  action..";
+        }
+
+      
+
+        public string Welcome2(string name, int id = 1)
+        {
+            return HttpUtility.HtmlDecode("HELLO" + name + "id" + id);
+
+        }
+        public ActionResult Index() {
+            return View  ();
+        
+        }
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "hello" + name;
+            ViewBag.numTimes = numTimes;
+
+
+   return View();
+        }
+
+
+    }
+}
---
+++ prueba/Controllers/listaMovie.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prueba.Controllers
+{
+    public class listaMovie : Controller
+    {
+        // GET: listaMovie
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: listaMovie/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: listaMovie/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: listaMovie/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: listaMovie/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: listaMovie/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: listaMovie/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: listaMovie/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
